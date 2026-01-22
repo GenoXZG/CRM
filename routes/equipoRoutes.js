330082d2da1c1171621ec8de0e49083745b8ed50
Luis Zarate <126049010+GenoXZG@users.noreply.github.com>
2026-01-21 19:02:09 -0600

    Update equipoRoutes.js

--- routes/equipoRoutes.js
+++ routes/equipoRoutes.js
@@ -1,4 +1,4 @@
-// routes/equipoRoutes.js
+
 const express = require('express');
 const router = express.Router();
 
@@ -6,23 +6,22 @@ const router = express.Router();
 const equipo_catalogo_controller = require("../controllers/catalogo_equipos_controller");
 const Equipo_controller = require('../controllers/equipos_controller');
 
-// Servicios (Para la vista combinada de asignación)
+// Servicios 
 const ticketService = require('../services/ticketService');
 const equipoService = require('../services/equipoService');
 const accesorioService = require('../services/catalogoAccesorioService');
 
-// === SECCIÓN A: CATÁLOGO (Stock/Modelos) ===
+//  SECCIÓN A: CATÁLOGO 
 router.get('/equipos', equipo_catalogo_controller.listarEquiposStock);
 router.post('/add_equipo_catalogo', equipo_catalogo_controller.agregarEquipoStock);
 router.post('/editar_equipo_stock', equipo_catalogo_controller.editarEquipoStock);
 router.post('/eliminar_equipo_stock', equipo_catalogo_controller.eliminarEquipoStock);
 
 
-// === SECCIÓN B: INVENTARIO (Equipos Reales) ===
+// SECCIÓN B: INVENTARIO 
 router.get("/registro_equipos", Equipo_controller.viewRegistroEquipos);
 router.post('/add_equipo', Equipo_controller.agregarEquipo);
 
-// API JSON
 router.get('/equipos_espera', Equipo_controller.apiEquiposEnEspera);
 
 
