099645211eff5f93fada74087a8e5662d27d4d9c
Luis Zarate <126049010+GenoXZG@users.noreply.github.com>
2026-01-21 19:03:02 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-// routes/index.js
+
 const express = require('express');
 const router = express.Router();
 const dashboard_controller = require('../controllers/dashboard_controller');
@@ -14,9 +14,6 @@ const accesorioRoutes = require('./accesorioRoutes');
 // Ruta Home
 router.get('/', dashboard_controller.viewDashboard);
 
-// Montaje de rutas
-// Usamos '/' para que las rutas definidas en los archivos se mantengan igual
-// (ej: '/clientes' en clienteRoutes seguirá siendo '/clientes')
 router.use('/', clienteRoutes);
 router.use('/', tecnicoRoutes);/*  */
 router.use('/', ticketRoutes);
