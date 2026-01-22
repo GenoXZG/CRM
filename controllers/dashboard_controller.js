ff6d6acd559dbbcae68f0b31105b92015ea91371
Luis Zarate <126049010+GenoXZG@users.noreply.github.com>
2026-01-21 18:48:59 -0600

    dashboard_controller.js

--- controllers/dashboard_controller.js
+++ controllers/dashboard_controller.js
@@ -2,7 +2,6 @@ const dashboardService = require('../services/dashboardService');
 
 const viewDashboard = async (req, res) => {
     try {
-        // Ejecutamos ambas consultas en paralelo para ser más rápidos
         const [metrics, calendarEvents] = await Promise.all([
             dashboardService.getDashboardMetrics(),
             dashboardService.getCalendarEvents()
